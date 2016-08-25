cd6744374f8d88c85ef259dca4e545b3bd437084
antklim <antklim@gmail.com>
2016-08-25 11:24:35 +1000

    Counter module app

---
+++ counter-app.elm
@@ -0,0 +1,58 @@
+import Html exposing (..)
+import Html.App as CounterApp
+import Html.Events exposing (onClick)
+
+import Counter
+
+main =
+  CounterApp.beginnerProgram
+    { model = init 0 0
+    , view = view
+    , update = update
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { topCounter : Counter.Model
+  , bottomCounter : Counter.Model
+  }
+
+init : Int -> Int -> Model
+init top bottom =
+  { topCounter = Counter.init top
+  , bottomCounter = Counter.init bottom
+  }
+
+
+-- UPDATE
+
+type Msg
+  = Reset
+  | Top Counter.Msg
+  | Bottom Counter.Msg
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Reset ->
+      init 0 0
+
+    Top msg ->
+      { model | topCounter = Counter.update msg model.topCounter }
+
+    Bottom msg ->
+      { model | bottomCounter = Counter.update msg model.bottomCounter }
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ CounterApp.map Top (Counter.view model.topCounter)
+    , CounterApp.map Bottom (Counter.view model.bottomCounter)
+    , button [ onClick Reset ] [ text "Reset" ]
+    ]
---
+++ counter.elm
@@ -0,0 +1,52 @@
+module Counter exposing ( Model, Msg, init, update, view )
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
+-- MODEL
+
+type alias Model = Int
+
+init : Int -> Model
+init count =
+  count
+
+
+-- UPDATE
+
+type Msg
+  = Increment
+  | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Increment ] [ text "+" ]
+    , div [ countStyle ] [ text (toString model) ]
+    , button [ onClick Decrement ] [ text "-" ]
+    ]
+
+
+countStyle : Attribute msg
+countStyle =
+  style
+  [ ("font-size", "20px")
+  , ("font-family", "monospace")
+  , ("display", "inline-block")
+  , ("width", "50px")
+  , ("text-align", "center")
+  ]
